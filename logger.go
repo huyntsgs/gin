cffff67bad96f6e874178a82a6ab24ecaa5d665a
huyntsgs <huykbc@gmail.com>
2018-03-03 08:49:27 +0700

    Update logger.go

--- logger.go
+++ logger.go
@@ -107,7 +107,7 @@ func LoggerWithWriterInfo(out io.Writer, userInfo string, notlogged ...string) H
 			}
 
 			if c.GetString(userInfo) != "" {
-				fmt.Fprintf(out, "[GIN] %v |%s %3d %s| %s| %13v | %15s |%s %-7s %s %s\n%s",
+				fmt.Fprintf(out, "[GIN] %v |%s %3d %s | %s| %13v | %15s |%s %-7s %s %s\n%s",
 				end.Format("2006/01/02 - 15:04:05"),
 				statusColor, statusCode, resetColor,
 				latency,
